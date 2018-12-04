eaddf76a5e8331a1e318e311361c86831153bf2d
Xan <xan@XanDev.local>
2018-12-04 16:04:07 +0800

    domain bug fixed

--- info_gather_engine.rb
+++ info_gather_engine.rb
@@ -24,14 +24,15 @@ class PortScanEngine
 				ranges.each do |range|
 					data << range.to_s
 				end
-			else
+			elsif target =~ /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
 				resolvs = Resolv.getaddresses(target)
 				resolvs.each do |resolv|
 					data << resolv
 				end
+			else
+				data << target
 			end
 		end
-		data << @targets.join()
 		return data
 	end
 
